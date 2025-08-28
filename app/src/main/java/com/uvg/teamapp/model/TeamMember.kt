19db5fb3ed484b9ea729b17cc91023aaa42e8246
Adrian-24914 <pen24914@uvg.edu.gt>
2025-08-28 16:34:36 -0600

    add TeamMember data calss

---
+++ app/src/main/java/com/uvg/teamapp/model/TeamMember.kt
@@ -0,0 +1,12 @@
+package com.example.teamapp.model
+
+/**
+ * Representa a un miembro del equipo.
+ *
+ * @param name Nombre del integrante.
+ * @param description Breve descripción del integrante.
+ */
+data class TeamMember(
+    val name: String,
+    val description: String
+)
