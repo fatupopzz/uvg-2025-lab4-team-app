8df4975bf69dcc2c4dd87f7da253c0bf97cd1278
fatitaprogramando <nav24044@uvg.edu.gt>
2025-08-28 21:00:24 -0600

    feat: integrate navigation in MainActivity
    - Replace direct screen usage with navigation
    - Enable full app navigation flow
    - Maintain clean activity structure

--- app/src/main/java/com/uvg/teamapp/MainActivity.kt
+++ app/src/main/java/com/uvg/teamapp/MainActivity.kt
@@ -5,25 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.uvg.teamapp.ui.screens.TeamListScreen
+import com.uvg.teamapp.ui.navigation.AppNavigation
 import com.uvg.teamapp.ui.theme.TeamAppTheme
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             TeamAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TeamListScreen(
-                        onMemberClick = { member ->
-                            println("Clicked on: ${member.name}")
-                        },
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation(modifier = Modifier.fillMaxSize())
             }
         }
     }
